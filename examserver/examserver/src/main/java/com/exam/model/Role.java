6af4efbd32fc0f7684eace42a75bdbbdef152e18
somnath <s.paulsomnath04@gmail.com>
2025-10-12 23:23:28 +0530

    created User,Role Entity

---
+++ examserver/examserver/src/main/java/com/exam/model/Role.java
@@ -0,0 +1,71 @@
+package com.exam.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="roles")
+public class Role {
+	
+	@Id
+	private Long roleId;
+	private Long roleName;
+	
+	@OneToMany(cascade = CascadeType.ALL,fetch=FetchType.LAZY,mappedBy = "role")
+	private Set<UserRole> userRoles=new HashSet<>();
+	
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Role(Long roleId, Long roleName) {
+		super();
+		this.roleId = roleId;
+		this.roleName = roleName;
+	}
+
+
+	public Long getRoleId() {
+		return roleId;
+	}
+
+
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+
+
+	public Long getRoleName() {
+		return roleName;
+	}
+
+
+	public void setRoleName(Long roleName) {
+		this.roleName = roleName;
+	}
+
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+	
+	
+	
+	
+	
+
+}
