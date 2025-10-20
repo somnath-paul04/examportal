6bbcec795b4c29202d83859acdca4ff780951cfc
somnath <s.paulsomnath04@gmail.com>
2025-10-21 01:05:46 +0530

    Updated User class and implemented UserDetails Interface

--- examserver/examserver/src/main/java/com/exam/model/User.java
+++ examserver/examserver/src/main/java/com/exam/model/User.java
@@ -1,7 +1,11 @@
 package com.exam.model;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -16,8 +20,10 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="users")
-public class User {
-	
+public class User implements UserDetails {
+
+
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
@@ -135,6 +141,40 @@ public class User {
 
 	public void setUserRoles(Set<UserRole> userRoles) {
 		this.userRoles = userRoles;
+	}
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		
+		Set<Authority> set =new HashSet<>();
+		this.userRoles.forEach(userRole->{
+			set.add(new Authority(userRole.getRole().getRoleName()));
+		});
+		
+		return null;
+	}
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 	
 	
