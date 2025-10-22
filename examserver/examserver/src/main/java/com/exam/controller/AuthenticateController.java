dc41206d5c9245054e0a2a72819b75d88a932061
somnath <s.paulsomnath04@gmail.com>
2025-10-22 15:02:51 +0530

    added a controller for authentication and generate token

--- examserver/examserver/src/main/java/com/exam/controller/AuthenticateController.java
+++ examserver/examserver/src/main/java/com/exam/controller/AuthenticateController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import com.exam.model.JwtResponse;
 import com.exam.service.impl.UserDetailsServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class AuthenticateController {
 
 	@Autowired
