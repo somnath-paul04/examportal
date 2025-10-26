c627f0fa5710dac9082cad0b6964aff5b4a7c2d9
opnayak20 <ommprakashnayak1305@gmail.com>
2025-10-26 20:31:07 +0530

    added logic for the logout button

--- examfront/src/app/components/navbar/navbar.ts
+++ examfront/src/app/components/navbar/navbar.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { SharedMaterialImports } from '../../shared/shared-material';
 import { RouterLink } from "@angular/router";
+import { LoginService } from '../../services/login';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [SharedMaterialImports, RouterLink],
+  imports: [SharedMaterialImports, RouterLink,CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  constructor(public login:LoginService){}
+
+  public logout(){
+    this.login.logout();
+    window.location.reload();
+  }
 
 }
