2fe89973f99010a315fc5aaf3b069e331efc4b57
opnayak20 <ommprakashnayak1305@gmail.com>
2025-10-26 20:33:26 +0530

    added navigation logic after submitting correct details

--- examfront/src/app/pages/login/login.ts
+++ examfront/src/app/pages/login/login.ts
@@ -3,56 +3,71 @@ import { SharedMaterialImports } from '../../shared/shared-material';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { JsonPipe } from '@angular/common';
 import { LoginService } from '../../services/login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [SharedMaterialImports,JsonPipe],
+  standalone:true,
+  imports: [SharedMaterialImports, JsonPipe],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
 export class Login {
   user: any;
   loginData = {
     userName: '',
     password: ''
-  }
-  constructor(private snack:MatSnackBar,private loginService:LoginService) {
+  };
+  login: any;
 
-  }
-  // ngOnit():void{}
+  constructor(
+    private snack: MatSnackBar,
+    private loginService: LoginService,
+    private router: Router
+  ) {}
+
   formSubmit() {
-    console.log("Login btn clicked ");
+    console.log("Login btn clicked");
 
-    if(this.loginData.userName.trim()=='' || this.loginData.userName==null){
-      this.snack.open('Username is required...!!','',{
-        duration:3000,
-      });
-
+    if (this.loginData.userName.trim() == '' || this.loginData.userName == null) {
+      this.snack.open('Username is required...!!', '', { duration: 3000 });
       return;
-
     }
-    if(this.loginData.password.trim()=='' || this.loginData.password==null){
-      this.snack.open('Password is required...!!','',{
-        duration:3000,
-      });
 
+    if (this.loginData.password.trim() == '' || this.loginData.password == null) {
+      this.snack.open('Password is required...!!', '', { duration: 3000 });
       return;
-
     }
-    //request to server to generate token
+
     this.loginService.generateToken(this.loginData).subscribe(
-      (data:any)=>{
+      (data: any) => {
         console.log('Success');
         console.log(data);
+
+        this.loginService.loginUser(data.token);
+
+        this.loginService.getCurrentUser().subscribe(
+          (user: any) => {
+            this.loginService.setUser(user);
+            console.log(user);
+
+            if (this.loginService.getUserRole() == "ADMIN") {
+              this.router.navigate(['/admin']);
+            } else if (this.loginService.getUserRole() == 'NORMAL') {
+              this.router.navigate(['/user-dashboard']);
+            } else {
+              this.loginService.logout();
+            }
+          }
+        );
       },
-      (error)=>{
+      (error) => {
         console.log('Error !');
         console.log(error);
+        this.snack.open("Invalid Details !! Try again",'',{
+          duration:3000,
+        });
       }
-
     );
-    
   }
-
-
 }
