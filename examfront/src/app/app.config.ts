9791ab2b1ec715a7f73ac06ed3b7ca78f3a89d48
opnayak20 <ommprakashnayak1305@gmail.com>
2025-10-26 20:25:20 +0530

    added authentication interceptors

--- examfront/src/app/app.config.ts
+++ examfront/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient,withInterceptorsFromDi } from '@angular/common/http';
+import { authInterceptorProviders } from './services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    authInterceptorProviders,
   ]
 };
