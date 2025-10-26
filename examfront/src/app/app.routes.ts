507a3ef14236bfea2419371daa5d7027bb0ce014
opnayak20 <ommprakashnayak1305@gmail.com>
2025-10-26 20:28:39 +0530

    created normal & admin dashboard and protects with a guard

--- examfront/src/app/app.routes.ts
+++ examfront/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import { Signup } from './pages/signup/signup';
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
+import { Dashboard } from './pages/admin/dashboard/dashboard';
+import { UserDashboard } from './pages/user/user-dashboard/user-dashboard';
+import { adminGuard } from './services/admin-guard';
+import { normalGuard } from './services/normal-guard';
 
 export const routes: Routes = [
     {
@@ -17,5 +21,17 @@ export const routes: Routes = [
         path:'login',
         component:Login,
         pathMatch:'full',
-    }
+    },
+    {
+        path:'admin',
+        component: Dashboard,
+        pathMatch: 'full',
+        canActivate:[adminGuard],
+    },
+    {
+        path:'user-dashboard',
+        component: UserDashboard,
+        pathMatch:'full',
+        canActivate : [normalGuard],
+    },
 ];
