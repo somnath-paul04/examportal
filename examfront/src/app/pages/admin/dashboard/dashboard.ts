6dd8331c1d9b781f77eba5a00ce473186b78b7ed
somnath <s.paulsomnath04@gmail.com>
2025-10-29 01:35:21 +0530

    designed admin dashboard

--- examfront/src/app/pages/admin/dashboard/dashboard.ts
+++ examfront/src/app/pages/admin/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedMaterialImports } from '../../../shared/shared-material';
+import { Sidebar } from "../sidebar/sidebar";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [SharedMaterialImports, Sidebar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
