d2649afd505c729d258a63156ab7985d75848651
somnath <s.paulsomnath04@gmail.com>
2025-11-05 15:34:56 +0530

    created an endpoint api for fetching all questions data

--- examserver/examserver/src/main/java/com/exam/controller/QuestionController.java
+++ examserver/examserver/src/main/java/com/exam/controller/QuestionController.java
@@ -26,59 +26,66 @@ import com.exam.service.QuizService;
 @CrossOrigin(origins = "*")
 @RequestMapping("/question")
 public class QuestionController {
-	
+
 	@Autowired
 	private QuestionService service;
-	
+
 	@Autowired
 	private QuizService quizService;
-	
-	//add question
+
+	// add question
 	@PostMapping("/")
-	public ResponseEntity<Question> add(@RequestBody Question question){
+	public ResponseEntity<Question> add(@RequestBody Question question) {
 		return ResponseEntity.ok(this.service.addQuestion(question));
 	}
-	
-	//update the question
+
+	// update the question
 	@PutMapping("/")
-	public ResponseEntity<Question> update(@RequestBody Question question){
+	public ResponseEntity<Question> update(@RequestBody Question question) {
 		return ResponseEntity.ok(this.service.updateQuestion(question));
 	}
+
 	
-	//get all question of any quiz
 	@GetMapping("/quiz/{qid}")
-	public ResponseEntity<?> getQuestionsOfQuiz(@PathVariable("qid") Long qid){
+	public ResponseEntity<?> getQuestionsOfQuiz(@PathVariable("qid") Long qid) {
 //		Quiz quiz=new Quiz();
 //		quiz.setqId(qid);
 //		Set<Question> questionsOfQuiz = this.service.getQuestionsOfQuiz(quiz);
 //		return ResponseEntity.ok(questionsOfQuiz);
-		
-		
-		Quiz quiz=this.quizService.getQuiz(qid);
+
+		Quiz quiz = this.quizService.getQuiz(qid);
 		Set<Question> questions = quiz.getQuestions();
-		
-		List list=new ArrayList<>(questions);
-		if(list.size()>Integer.parseInt(quiz.getNumberOfQuestions())) {
-			list=list.subList(0, Integer.parseInt(quiz.getNumberOfQuestions()+1));
-			
+
+		List list = new ArrayList<>(questions);
+		if (list.size() > Integer.parseInt(quiz.getNumberOfQuestions())) {
+			list = list.subList(0, Integer.parseInt(quiz.getNumberOfQuestions() + 1));
+
 		}
 		Collections.shuffle(list);
-		return ResponseEntity.ok(list);	
+		return ResponseEntity.ok(list);
 	}
-	
-	//get single question
+
+	// get all question of any quiz
+	@GetMapping("/quiz/all/{qid}")
+	public ResponseEntity<?> getQuestionsOfQuizAdmin(@PathVariable("qid") Long qid) {
+		Quiz quiz = new Quiz();
+		quiz.setqId(qid);
+		Set<Question> questionsOfQuiz = this.service.getQuestionsOfQuiz(quiz);
+		return ResponseEntity.ok(questionsOfQuiz);
+
+	}
+
+	// get single question
 	@GetMapping("/{quesId}")
 	public Question get(@PathVariable("quesId") Long quesId) {
 		return this.service.getQuestion(quesId);
-		
-		
+
 	}
-	
-	//delete  question
+
+	// delete question
 	@DeleteMapping("/{quesId}")
 	public void delete(@PathVariable("quesId") Long quesId) {
 		this.service.deletequestion(quesId);
 	}
-	
 
 }
